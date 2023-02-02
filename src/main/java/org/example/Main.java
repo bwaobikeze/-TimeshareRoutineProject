2b59ce161061dcccaf715557ee9ffc879bc64e68
bwaobikeze <bwaobikeze@hotmail.com>
2023-02-02 14:21:52 -0600

    adding core function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //System.out.println("Hello world!");
         processingRoutine beginProcess= new processingRoutine();
         beginProcess.ArrivleTime(200,2);
     }
--- src/main/java/org/example/processingRoutine.java
+++ src/main/java/org/example/processingRoutine.java
@@ -3,16 +3,40 @@ package org.example;
 import java.util.ArrayList;
 
 public class processingRoutine {
+    int time=0;
     int numberOfCores;
     ArrayList<process>ProcessList = new ArrayList<>();
+    ArrayList<core>CoreList= new ArrayList<>();
 
     void RoutineLoop(){
 
     }
     void ArrivleTime(int ArrivleTime, int ProcessId){
-        System.out.println("ArrivleTime Function");
+        System.out.println("Process "+ProcessId+" starts at t="+ArrivleTime+"ms");
+
     }
-    void coreRequest(int ArrivleTime, int ProcessId){
+    boolean CoreAvailablity(){
+        for( int i=0; i< CoreList.size();i++){
+            if(CoreList.get(i).availabilty==true){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void coreRequest(process CoreProcess){
+        ArrayList<process> coreReadyQueue= new ArrayList<>();
+        if(CoreAvailablity()==true){
+            if(!coreReadyQueue.isEmpty()){
+                    numberOfCores--;
+                    process coreProcesPop=coreReadyQueue.remove(0);
+                    time=time+coreProcesPop.timeRequest;
+        }
+        else{
+            coreReadyQueue.add(CoreProcess);
+        }
+
+
 
         System.out.println("coreRequest Function");
     }
