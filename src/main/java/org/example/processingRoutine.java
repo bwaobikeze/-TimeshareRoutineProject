f6b1e5b6311d9c9b93af3ccb76fefb5c50d1b6be
bwaobikeze <bwaobikeze@hotmail.com>
2023-02-04 03:13:20 -0600

    Added Routine Function

--- src/main/java/org/example/processingRoutine.java
+++ src/main/java/org/example/processingRoutine.java
@@ -10,15 +10,43 @@ public class processingRoutine {
     int numberOfCores;
     ArrayList<process>ProcessList = new ArrayList<>();
     ArrayList<core>CoreList= new ArrayList<>();
+
+    ArrayList<process> coreReadyQueue= new ArrayList<>();
     void CreateProcessObj(String read){
-        System.out.println(read);
+        System.out.println(read.charAt(0));
+
     }
 
     void RoutineLoop(){
+        while(!ProcessList.isEmpty()){
+            process currentProcess=ProcessList.remove(0);
+            if(currentProcess.subProcessName=="START"){
+                ArrivleTime(currentProcess);
+            }
+            else if(currentProcess.subProcessName=="CPU"){
+                coreRequest(currentProcess);
+            }
+        }
 
     }
-    void ArrivleTime(int ArrivleTime, int ProcessId){
-        System.out.println("Process "+ProcessId+" starts at t="+ArrivleTime+"ms");
+    void ArrivleTime(process arrivePro){
+        int busyCores=0;
+        String processInReadyQueue="";
+        for(int i =0; i<CoreList.size(); i++){
+            if(CoreList.get(i).availabilty==false){
+                busyCores++;
+            }
+        }
+        for(int j=0; j<coreReadyQueue.size();j++){
+            processInReadyQueue+=Integer.toString(coreReadyQueue.get(j).processNum);
+            processInReadyQueue+=",";
+
+        }
+        System.out.println("Process "+arrivePro.processNum+" starts at t="+arrivePro.timeRequest+"ms");
+        System.out.println("Current number of busy cores: "+busyCores);
+        System.out.println("Ready Queue has Processes "+processInReadyQueue);
+
+
 
     }
     boolean CoreAvailablity(){
@@ -31,7 +59,7 @@ public class processingRoutine {
     }
 
     void coreRequest(process CoreProcess){
-        ArrayList<process> coreReadyQueue= new ArrayList<>();
+
 
 
 
