85e612cfd54d635e1fbe0005435368bdb610a31d
Brian Waobikeze <48830488+bwaobikeze@users.noreply.github.com>
2023-02-11 11:41:52 -0600

    Delete LockObj.java

--- src/main/java/org/example/LockObj.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import java.util.ArrayList;
-
-public class LockObj {
-    boolean availabilty;
-    int SubProcessDoneAt;
-    ArrayList<SubProcess> LockReadyQueue= new ArrayList<>();
-}
